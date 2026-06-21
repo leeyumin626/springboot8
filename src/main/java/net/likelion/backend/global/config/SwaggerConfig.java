12c1e7a95e1635afb3f9db5260c3aa570b2d20c6
leeyumin626 <leeym7404@naver.com>
2026-06-22 00:15:25 +0900

    feat: security부분 추가

--- src/main/java/net/likelion/backend/global/config/SwaggerConfig.java
+++ src/main/java/net/likelion/backend/global/config/SwaggerConfig.java
@@ -3,18 +3,37 @@ package net.likelion.backend.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public OpenAPI openAPI() {
-		return new OpenAPI()
-			.info(new Info()
-				.title("멋사 백엔드 API")
-				.description("Spring Boot 실습 API 문서")
-				.version("v1.0.0"));
-	}
+    @Bean
+    public OpenAPI openAPI() {
+
+        String jwtSchemeName = "JWT";
+
+        SecurityRequirement securityRequirement =
+                new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("멋사 백엔드 API")
+                        .description("Spring Boot 실습 API 문서")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
